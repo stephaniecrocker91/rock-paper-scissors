d82d5f18105974397cd7cb046bc57fb5105c3167
Stephie <stephaniecrocker91@gmail.com>
2022-01-29 14:03:02 +0000

    Add code to increment WIN score by 1 each time user wins

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 // Gets button selection-elements and adds event listeneres
 let selectionButton = document.getElementsByClassName("selection-button");
+//Gets alert-result section where "You win", "You lose" or "You draw" will be displayed accordingly.
+let alertResult = document.getElementById("alert-result");
+
+//Gets span where WIN score will be displayed.
+let wins = document.getElementById("user-win");
 
 //Where we store our user's choice
 let userSelectedChoice;
@@ -20,6 +25,7 @@ function userChoice(e) {
     console.log(`user chose ${userSelectedChoice}`);
     computerChoice();
     console.log(`computer chose ${computerSelectedChoice}`);
+    compareChoice();
 }
 
 function runGame() {
@@ -42,13 +48,30 @@ function computerChoice() {
         }
 
 }
-
+/** Function that compares user choice and computer choice, determining
+ * the result and displaying an alert message below: ("You win!", "You lose!" or "You draw!") */ 
 function compareChoice() {
-
+    if (computerSelectedChoice===userSelectedChoice) {
+        alertResult.innerHTML = "You draw!";
+        updateDraw;
+    } else if ((userSelectedChoice==="paper" && computerSelectedChoice==="rock") 
+    || (userSelectedChoice==="rock" && computerSelectedChoice==="scissors") 
+    || (userSelectedChoice==="scissors" && computerSelectedChoice==="paper")) {
+        alertResult.innerHTML = "You win!";
+        updateWin();
+    } else if ((userSelectedChoice==="paper" && computerSelectedChoice==="scissors") 
+    || (userSelectedChoice==="rock" && computerSelectedChoice==="paper") 
+    || (userSelectedChoice==="scissors" && computerSelectedChoice==="rock")) {
+        alertResult.innerHTML = "You lose!";
+        updateLose();
+    } 
 }
-
+/** Function that increments the WIN
+ * score by 1 point, each time user wins
+ */
 function updateWin() {
-
+    oldScore = parseInt(wins.innerText);
+    document.getElementById("user-win").innerText = oldScore+1;
 }
 
 function updateDraw() {
