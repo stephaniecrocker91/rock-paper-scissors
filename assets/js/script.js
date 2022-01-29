02137f650e73b7c642e1410fcf730ecf9737a2af
Stephie <stephaniecrocker91@gmail.com>
2022-01-29 12:33:06 +0000

    Add click event listener to selection button and create a function that selects target id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,18 @@
-function runGame() {
+// Gets button selection-elements and adds event listeneres
+let selectionButton = document.getElementsByClassName("selection-button");
+let userSelectedChoice;
 
+
+for (i of selectionButton) {
+    i.addEventListener("click", userChoice);
 }
 
-function userChoice() {
+function userChoice(e) {
+   userSelectedChoice = e.target.id;
+    console.log(userSelectedChoice);
+}
+
+function runGame() {
 
 }
 
@@ -27,5 +37,5 @@ function updateLose() {
 }
 
 function gameOver() {
-    
+
 }
